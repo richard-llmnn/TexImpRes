d8d15388c2539086b94e3065a91c8a1d97152385
rullmann <69484873+richard-llmnn@users.noreply.github.com>
2022-07-13 20:36:13 +0200

    add @import escaping support

--- src/SemanticFunctions/Import/ImportResolver.py
+++ src/SemanticFunctions/Import/ImportResolver.py
@@ -1,43 +1,54 @@
 import Semantic.ImportSemantic as ImportSemantic
 import os.path
 
+
 class ImportResolver:
     def __init__(self, entry_file_path, output_file_path):
         self.entry_file_path = entry_file_path
         self.output_file_path = output_file_path
 
     def run(self):
-
-        before_file_path = self.entry_file_path
-
         with open(self.entry_file_path, "r") as entry_file:
             file_content = entry_file.read()
 
-            while True:
-                found_import = ImportSemantic.ImportSemantic(file_content).get_first()
-                if found_import is None:
-                    break
-
-                print(f"---Found import: {found_import.group()}---")
-                if file_content[found_import.start() - 1] == "\\" and file_content[found_import.start() - 2] != "\\":
-                    continue
-
-                import_rel_path = found_import.group().split('"')[-2]
-                import_abs_path = self.create_rel_path(before_file_path, import_rel_path)
-                before_file_path = import_abs_path
-
-                if not os.path.exists(import_abs_path):
-                    import_abs_path = self.create_rel_path(self.entry_file_path, import_rel_path)
-                    before_file_path = import_abs_path
-
-                file = open(import_abs_path, "r")
-                import_file_content = file.read()
-                file.close()
-
-                file_content = file_content[0:found_import.start()] + import_file_content + file_content[found_import.end():]
+        file_content = self.recursive_resolve_files(file_content, self.entry_file_path)
+        file_content = self.remove_escaped_imports(file_content)
 
         with open(self.output_file_path, "w") as output_file:
             output_file.write(file_content)
+
+    def remove_escaped_imports(self, file_content):
+        found_imports = list(ImportSemantic.ImportSemantic(file_content).get_all())
+        found_imports.reverse()
+
+        for found_import in found_imports:
+            if file_content[found_import.start() - 1] == "\\" and file_content[found_import.start() - 2] != "\\":
+                file_content = file_content[:found_import.start() - 1] + file_content[found_import.start():]
+
+
+        return file_content
+
+    def recursive_resolve_files(self, file_content, current_abs_path):
+        found_imports = list(ImportSemantic.ImportSemantic(file_content).get_all())
+        found_imports.reverse()
+
+        for found_import in found_imports:
+            if file_content[found_import.start() - 1] == "\\" and file_content[found_import.start() - 2] != "\\":
+                continue
+            import_rel_path = found_import.group().split('"')[-2]
+            import_abs_path = self.create_rel_path(current_abs_path, import_rel_path)
+
+            file = open(import_abs_path, "r")
+            import_file_content = self.recursive_resolve_files(file.read(), import_abs_path)
+            file.close()
+
+            offset = 0
+            if file_content[found_import.start() - 2 : found_import.start()] == "\\\\":
+                offset = 1
+
+            file_content = file_content[0:found_import.start() - offset] + import_file_content + file_content[found_import.end():]
+
+        return file_content
 
     def create_rel_path(self, root_path, file_path):
         return os.path.abspath(os.path.join(os.path.dirname(root_path), file_path))
